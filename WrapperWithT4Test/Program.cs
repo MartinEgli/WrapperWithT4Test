37cf16769685156009a601b2719c947827857be6
Martin <Martin@eglinet.ch>
2020-09-13 20:03:54 +0200

    Add project files.

---
+++ WrapperWithT4Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WrapperWithT4Test
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var x = new Siemens.Sinumerik.Operate.Services.AlarmSvc("");
+        }
+    }
+}
